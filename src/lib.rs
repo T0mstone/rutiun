8a516a9913e1eb7517d0ec4bece62722985cb259
T0mstone <realt0mstone@gmail.com>
2021-07-10 03:11:29 +0200

    Improve si alias module

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,10 @@ pub type Rational = num_rational::Ratio<Integer>;
 pub mod si {
 	pub use crate::quantities::isq::*;
 	pub use crate::unit::si::*;
-	pub type SiValue<T, U> = crate::value::Value<SI, T, U>;
+	pub type SimpleUnit = crate::unit::SimpleUnit<SI>;
+	pub type ScalableUnit<T> = crate::unit::ScalableUnit<SI, T>;
+	pub type AffineUnit<T, O> = crate::unit::AffineUnit<SI, T, O>;
+	pub type Value<T, U> = crate::value::Value<SI, T, U>;
 }
 
 mod composite;
