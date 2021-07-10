6c754161cce550940d8366aabf95e4c84e87e946
T0mstone <realt0mstone@gmail.com>
2021-07-10 03:03:39 +0200

    Add si alias module

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,12 @@ pub type Integer = i64;
 pub type Integer = num_bigint::BigInt;
 pub type Rational = num_rational::Ratio<Integer>;
 
+pub mod si {
+	pub use crate::quantities::isq::*;
+	pub use crate::unit::si::*;
+	pub type SiValue<T, U> = crate::value::Value<SI, T, U>;
+}
+
 mod composite;
 
 pub mod quantities;
